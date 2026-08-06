a7fbc54e0e95a96cdfc18088bbb82458159b3314
ritaban126 <ritaban.rjp@gmail.com>
2026-08-06 20:25:35 +0530

    change health worker.ts

--- apps/server/worker.ts
+++ apps/server/worker.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
+import express from "express";
 import {startImageProcessingWorker} from "./src/jobs/imageProcessing.job";
 import {startNotificationWorker} from "./src/jobs/notification.job";
 
 
+const healthApp = express();
+healthApp.get("/health", (_req, res) => res.json({ status: "worker alive" }));
+healthApp.listen(process.env.PORT || 3001);
 startImageProcessingWorker();
 startNotificationWorker();
 
