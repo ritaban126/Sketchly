23704987b1561d1bdba599a73fdda45191d88081
ritaban126 <ritaban.rjp@gmail.com>
2026-08-08 01:29:53 +0530

    Fix auth and from web auth-client.ts

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,7 +1,20 @@
+// "use client"
+// import { createAuthClient } from 'better-auth/client';
+// import { passkeyClient } from "@better-auth/passkey/client";
+
+// export const client = createAuthClient({
+//   plugins: [passkeyClient()],
+// });
+
+
 "use client"
 import { createAuthClient } from 'better-auth/client';
 import { passkeyClient } from "@better-auth/passkey/client";
 
 export const client = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  fetchOptions: {
+    credentials: "include",
+  },
   plugins: [passkeyClient()],
 });
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,20 +1,57 @@
-import { betterAuth } from "better-auth"
-import { drizzleAdapter } from "better-auth/adapters/drizzle"
+// import { betterAuth } from "better-auth";
+// import { drizzleAdapter } from "better-auth/adapters/drizzle";
+// import { db } from "@repo/db";
+// import * as schema from "@repo/db";
+
+
+// export const auth = betterAuth({
+//   database: drizzleAdapter(db, {
+//     provider: "pg", // PostgreSQL
+//     schema
+//   }),
+//   emailAndPassword: {
+//     enabled: true,
+//     requireEmailVerification: false
+//   },
+//   secret: process.env.BETTER_AUTH_SECRET!,
+//   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+//     trustedOrigins: [
+//     process.env.CLIENT_URL || "http://localhost:3000",
+//   ],
+// });
+
+// export type Session = typeof auth.$Infer.Session;
+
+
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@repo/db";
 import * as schema from "@repo/db";
 
-
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // PostgreSQL
-    schema
+    provider: "pg",
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: false
+    requireEmailVerification: false,
   },
   secret: process.env.BETTER_AUTH_SECRET!,
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
+  trustedOrigins: [
+    process.env.CLIENT_URL || "http://localhost:3000",
+  ],
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: false,
+    },
+    useSecureCookies: process.env.NODE_ENV === "production",
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+    },
+  },
 });
 
 export type Session = typeof auth.$Infer.Session;
